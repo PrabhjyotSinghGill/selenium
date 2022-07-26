9e2c5c56818cd1acdcb0c81875e29f77837e18a6
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-07-26 11:45:56 -0400

    setup browser preferences

--- driverConfig/index.js
+++ driverConfig/index.js
@@ -1,5 +1,11 @@
 const {Builder} = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+const options = new firefox.Options();
 
-const driver = new Builder().forBrowser("firefox").build();
+options.setPreference("browser.download.dir","C:\\mySeleniumDownloads");
+options.setPreference("browser.download.folderList",2);
+options.setPreference("browser.helperApps.neverAsk.saveToDisk","application/x-csv");
 
-driver.get("https://www.mbam.qc.ca/en/");
+const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+
+driver.get("https://facebook.com");
